3b5b3c53c040523a113c686f1152cc65c4d84c8a
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-02-08 23:22:30 +0900

    Update list3-29

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from 'react-dom';
+import Rect from './Rect';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  input = '';
+
+  msgStyle = {
+    fontSize: "20pt",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+  }
+  inputStyle = {
+    fontSize: "20px",
+    padding: "5px",
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "type your name: "
+    };
+    this.doCheck = this.doCheck.bind(this);
+  }
+
+  doCheck(event) {
+    alert(event.target.value + "は長すぎます");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React</h1>
+        <h2>{this.state.message}</h2>
+        <Message maxlength="10" onCheck={this.doCheck}></Message>
+      </div>
+    );
+  }
+}
+
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.doChange = this.doChange.bind(this);
+  }
+
+  doChange(e) {
+    if (e.target.value.length > this.props.maxlength) {
+      this.props.onCheck(e);
+      e.target.value = e.target.value.substr(0, this.props.maxlength);
+    }
+  }
+
+  render() {
+    return (
+      <input type="text" style={this.inputStyle} onChange={this.doChange} />
+    );
+  }
 }
 
 export default App;
