1cab479a60c631b535ae9b3f19d8fcba7a06811d
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-02-09 22:39:54 +0900

    Add change theme button in list3-32

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -18,18 +18,35 @@ let theme = {
   },
 };
 
-const ThemeContext = React.createContext(theme.dark);
+const ThemeContext = React.createContext(theme);
 
 class App extends Component {
-  static contextType = ThemeContext
+  static contextType = ThemeContext;
+
+  constructor(props) {
+    super(props);
+    this.changeTheme = this.changeTheme.bind(this);
+    this.state = {
+      isDark: true,
+    };
+  }
+
+  changeTheme(e) {
+    this.setState((state) => ({
+      isDark: !state.isDark,
+    }));
+  }
 
   render() {
     return (
-      <div style={this.context}>
+      <div style={this.context[!this.state.isDark ? 'light' : 'dark']}>
+        <button onClick={this.changeTheme}>
+          {this.state.isDark ? "To Light" : "To dark"}
+        </button>
         <Title value="Content page" />
         <Message value="This is Content sample." />
         <Message value="#これはテーマのサンプルです．" />
-      </div>
+      </div >
     );
   }
 }
