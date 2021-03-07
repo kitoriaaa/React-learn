6aced46588f9efd2540be453b63ab11997f241dd
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-07 23:09:24 +0900

    Update to use context for theme change

--- react_app_v2/src/App.js
+++ react_app_v2/src/App.js
@@ -1,60 +1,68 @@
 import React, { Component } from 'react';
 import "./App.css";
 
+let theme = {
+  light: {
+    styles: {
+      backgroundColor: "#f0f9ff",
+      color: "#00f",
+    },
+    head: "bg-primary text-white display-4 mb-4",
+    alert: "alert alert-primary my-3",
+    text: "text-primary m-3",
+    foot: "py-4",
+  },
+  dark: {
+    styles: {
+      backgroundColor: "#336",
+      color: "#eef",
+    },
+    head: "bg-secondary text-white display-4 mb-4",
+    alert: "alert alert-dark my-3",
+    text: "text-light m-3",
+    foot: "py-4",
+  }
+};
+
+const ThemeContext = React.createContext(theme.dark);
+
 class App extends Component {
-  input = '';
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "input form",
-      message: "type your name",
-      max: 10,
-    };
-    this.doCheck = this.doCheck.bind(this);
-  }
-
-  doCheck(event) {
-    alert(event.target.value + "は長すぎます．（最大" + this.state.max + "文字）");
-  }
+  static contextType = ThemeContext;
 
   render() {
     return (
-      <div>
-        <h1 className="bg-primary text-white display-4">React</h1>
+      <div style={this.context.styles}>
+        <h1 className={this.context.head}>React</h1>
         <div className="container">
-          <h4>{this.state.title}</h4>
-          <Message maxlength={this.state.max} onCheck={this.doCheck} />
+          <Title value="Content page" />
+          <Message value="This is Content sample." />
+          <Message value="※これはテーマのサンプルです．" />
+          <div className={this.context.foot}></div>
         </div>
       </div>
-    )
+    );
+  }
+}
+
+class Title extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    return (
+      <div className={this.context.alert}>
+        <h2 style={this.context.style}>{this.props.value}</h2>
+      </div>
+    );
   }
 }
 
 class Message extends Component {
-  li = {
-    fontSize: "14pt",
-    fontWeight: "bold",
-    color: "#090",
-  };
-
-  constructor(props) {
-    super(props);
-    this.doChange = this.doChange.bind(this);
-  }
-
-  doChange(e) {
-    if (e.target.value.length > this.props.maxlength) {
-      this.props.onCheck(e);
-      e.target.value = e.target.value.substr(0, this.props.maxlength);
-    }
-  }
+  static contextType = ThemeContext;
 
   render() {
     return (
-      <div className="form-group">
-        <label>input:</label>
-        <input type="text" className="form-control" onChange={this.doChange} />
+      <div style={this.context.style}>
+        <p className={this.context.text}>{this.props.value}</p>
       </div>
     )
   }
