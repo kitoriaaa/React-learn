d3676c156b53545dfe5a702f54fc11c4306e9ea0
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-19 23:21:37 +0900

    Add: use SWR state

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,20 +1,32 @@
 import Layout from '../components/layout';
 import useSWR from 'swr';
 
-
 export default function Home() {
-  const fetcher = url => fetch(url).then(res => res.text());
-
-  const { data } = useSWR('http://localhost:8080/theme/current/', fetcher);
-  console.log(data);
+  const { data } = useSWR('/data.json');
 
   return (
-    <dvi>
-      <Layout header="Go api test" title="Go API test">
+    <div>
+      <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          {data}
+          <h5 className="mb-4">
+            {data != undefined ? data.message : 'error...'}
+          </h5>
+          <table className="table table-dark">
+            <thead className="">
+              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+            </thead>
+            <tbody>
+              {data != undefined ? data.data.map((value, key) => (
+                <tr key={key}>
+                  <th>{value.name}</th>
+                  <td>{value.mail}</td>
+                  <td>{value.age}</td>
+                </tr>
+              )) : <tr><th></th><td>no data.<td></td></td></tr>}
+            </tbody>
+          </table>
         </div>
       </Layout>
-    </dvi>
-  )
+    </div>
+  );
 }
