d37ab76510afce77243f8127fb90069c3ca27a45
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-20 00:14:32 +0900

    Add: fetch text data

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,30 +1,17 @@
 import Layout from '../components/layout';
-import useSWR from 'swr';
+import useSWR from 'swr'
 
 export default function Home() {
-  const { data } = useSWR('/data.json');
+  const fetcher = (...args) => fetch(...args).then(res => res.text());
+  const { data, err } = useSWR('./data.txt', fetcher);
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">
-            {data != undefined ? data.message : 'error...'}
+            {data}
           </h5>
-          <table className="table table-dark">
-            <thead className="">
-              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-            </thead>
-            <tbody>
-              {data != undefined ? data.data.map((value, key) => (
-                <tr key={key}>
-                  <th>{value.name}</th>
-                  <td>{value.mail}</td>
-                  <td>{value.age}</td>
-                </tr>
-              )) : <tr><th></th><td>no data.<td></td></td></tr>}
-            </tbody>
-          </table>
         </div>
       </Layout>
     </div>
